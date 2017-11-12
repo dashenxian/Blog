cc12a631440e71c8c6dbb13d3949748b7d7bb8e2
dashenxian <125880321@qq.com>
2017-11-12 19:38:53 +0800

    登录

--- Blog/Controllers/AccountController.cs
+++ Blog/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Blog.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace Blog.Controllers
 {
@@ -29,6 +30,28 @@ namespace Blog.Controllers
                 {
                     return RedirectToAction("Index", "Home");
                 }
+            }
+            return View(model);
+        }
+
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var userManager = new UserManager<IdentityUser, string>(new UserStore<IdentityUser>(new BlogIdentityDbContext()));
+            var sigInManager = new SignInManager<IdentityUser, string>(userManager, this.HttpContext.GetOwinContext().Authentication);
+            var result = await sigInManager.PasswordSignInAsync(model.UserName, model.Password, false, shouldLockout: false);
+            if (result == SignInStatus.Success)
+            {
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
             }
             return View(model);
         }
